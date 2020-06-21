5d0c42866576590db776ebd954c64e884c74ba7f
ezhaohengxin <ezhaohengxin@100tal.com>
2020-06-21 12:34:23 +0800

    测试

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,8 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div class="home">
+      小风博客
     </div>
-    <router-view/>
   </div>
 </template>
 
@@ -16,17 +14,11 @@
   text-align: center;
   color: #2c3e50;
 }
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+.home {
+  position: relative;
+  left: 50%;
+  top: 50%;
+  font-size: 36px;
+  transform: translate(-50%, -50%);
 }
 </style>
